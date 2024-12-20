a2606df31d611daebd5d9de860970366dc4f52aa
Shivam Pathak <shivampathak5042.com>
2024-12-21 00:32:03 +0530

    link should open in blank page

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -48,31 +48,51 @@ const Footer = ({ isSideVisible }) => {
           }`}
         >
           <figure className={Styles.iconRoom}>
-            <a href="">
+            <a
+              href="https://www.youtube.com/@shivamspathak"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaYoutube className={Styles.iconYT} />
             </a>
             <h5 className={Styles.figureCap}>YouTube</h5>
           </figure>
           <figure className={Styles.iconRoom}>
-            <a href="https://www.linkedin.com/in/shivam-s-pathak/">
+            <a
+              href="https://www.linkedin.com/in/shivam-s-pathak/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <SiLinkedin className={Styles.iconLI} />
             </a>
             <h5 className={Styles.figureCap}>LinkedIn</h5>
           </figure>
           <figure className={Styles.iconRoom}>
-            <a href="https://twitter.com/shivam_S_pathak">
+            <a
+              href="https://twitter.com/shivam_S_pathak"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <RiTwitterXLine className={Styles.iconX} />
             </a>
             <h5 className={Styles.figureCap}>X</h5>
           </figure>
           <figure className={Styles.iconRoom}>
-            <a href="https://github.com/Shivam-S-Pathak">
+            <a
+              href="https://github.com/Shivam-S-Pathak"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaGithub className={Styles.iconGH} />
             </a>
             <h5 className={Styles.figureCap}>Github</h5>
           </figure>
           <figure className={Styles.iconRoom}>
-            <a href="mailto:shivampathak5042@gmail.com">
+            <a
+              href="mailto:shivampathak5042@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <MdAlternateEmail className={Styles.iconMAIL} />
             </a>
             <h5 className={Styles.figureCap}>Mail</h5>
